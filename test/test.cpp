d4f394d501cd7d500a5cf676ed2fe2902d15841a
Alpha <PFA03027@nifty.com>
2023-02-18 15:21:47 +0900

    test: adapt CMake and googletest

--- test/test.cpp
+++ test/test.cpp
@@ -23,65 +23,42 @@ public:
 	int b;
 };
 
-bool test1( void )
+TEST( ObjectMutex, CanCall_default_constructor )
+{
+	// constructor check
+	obj_mutex<test_class1> tt1;
+
+	EXPECT_EQ( 10, tt1.lock_get().ref().a );
+	EXPECT_EQ( 11, tt1.lock_get().ref().b );
+
+	return;
+}
+
+TEST( ObjectMutex, CanCall_translate_constructor )
+{
+	// constructor check
+	obj_mutex<test_class1> tt1( 1, 2 );
+
+	EXPECT_EQ( 1, tt1.lock_get().ref().a );
+	EXPECT_EQ( 2, tt1.lock_get().ref().b );
+
+	return;
+}
+
+TEST( ObjectMutex, Locked )
 {
 	obj_mutex<test_class1> tt;
 
 	{
 		auto locked_data = tt.lock_get();
-
-		if ( tt.TEST_is_locked() ) {
-			// std::cout << "locked! OK -> this is expected. so, " << std::endl;
-		} else {
-			std::cout << "not locked! ERROR" << std::endl;
-			return false;
-		}
+		EXPECT_TRUE( tt.TEST_is_locked() );
 	}
-	if ( tt.TEST_is_locked() ) {
-		std::cout << "locked! ERROR" << std::endl;
-		return false;
-	} else {
-		// std::cout << "not locked! -> this is expected. so, OK" << std::endl;
-	}
+	EXPECT_FALSE( tt.TEST_is_locked() );
 
-	return true;
+	return;
 }
 
-bool test2_default_constructor( void )
-{
-	// constructor check
-	obj_mutex<test_class1> tt1;
-
-	if ( tt1.lock_get().ref().a != 10 ) {
-		std::cout << "ERROR: constructor check for member variable a" << std::endl;
-		return false;
-	}
-	if ( tt1.lock_get().ref().b != 11 ) {
-		std::cout << "ERROR: constructor check for member variable b" << std::endl;
-		return false;
-	}
-
-	return true;
-}
-
-bool test2_constructor_check( void )
-{
-	// constructor check
-	obj_mutex<test_class1> tt2( 1, 2 );
-
-	if ( tt2.lock_get().ref().a != 1 ) {
-		std::cout << "ERROR: constructor check for member variable a" << std::endl;
-		return false;
-	}
-	if ( tt2.lock_get().ref().b != 2 ) {
-		std::cout << "ERROR: constructor check for member variable b" << std::endl;
-		return false;
-	}
-
-	return true;
-}
-
-bool test3( void )
+TEST( ObjectMutex, RecusiveMutexLocked )
 {
 	obj_mutex<test_class1, std::recursive_mutex> tt3;
 
@@ -89,31 +66,17 @@ bool test3( void )
 		auto locked_data1 = tt3.lock_get();
 		// std::cout << "test log:" << std::to_string( locked_data1.ref().a ) << std::endl;
 
+		EXPECT_NE( 20, locked_data1.ref().a );
 		locked_data1.ref().a = 20;
 
-		if ( tt3.TEST_is_locked() ) {
-			std::cout << "ERROR: should be possible to lock" << std::endl;
-			return false;
-		} else {
-			// std::cout << "lockable additionally  -> this is expected. so, OK" << std::endl;
-		}
+		ASSERT_FALSE( tt3.TEST_is_locked() );
 
 		auto locked_data2 = tt3.lock_get();
-		if ( locked_data2.ref().a != 20 ) {
-			std::cout << "ERROR: should be same value. expected= 20, actual=" << std::to_string( locked_data2.ref().a ) << std::endl;
-			return false;
-		} else {
-			// std::cout << "lockable additionally  -> this is expected. so, OK" << std::endl;
-		}
+		EXPECT_EQ( 20, locked_data2.ref().a );
 	}
-	if ( tt3.TEST_is_locked() ) {
-		std::cout << "ERROR: should be possible to lock" << std::endl;
-		return false;
-	} else {
-		// std::cout << "lockable additionally -> this is expected. so,  OK" << std::endl;
-	}
+	ASSERT_FALSE( tt3.TEST_is_locked() );
 
-	return true;
+	return;
 }
 
 obj_mutex<test_class1>::single_accessor tttt( void )
@@ -122,37 +85,9 @@ obj_mutex<test_class1>::single_accessor tttt( void )
 	return tt1.lock_get();
 }
 
-bool test4( void )
+TEST( ObjectMutex, move_constructor_single_accessor )
 {
 	auto locked_data1 = tttt();
-	if ( locked_data1.ref().a != 10 ) {
-		std::cout << "ERROR: constructor check for member variable a" << std::endl;
-		return false;
-	}
-
-	return true;
+	EXPECT_EQ( 10, locked_data1.ref().a );
+	return;
 }
-
-// int main( void )
-// {
-// 	int exit_code = EXIT_SUCCESS;
-// 	if ( !test1() ) {
-// 		exit_code = EXIT_FAILURE;
-// 	}
-// 	if ( !test2_default_constructor() ) {
-// 		exit_code = EXIT_FAILURE;
-// 	}
-// 	if ( !test2_constructor_check() ) {
-// 		exit_code = EXIT_FAILURE;
-// 	}
-// 	if ( !test3() ) {
-// 		exit_code = EXIT_FAILURE;
-// 	}
-
-// 	if ( exit_code == EXIT_SUCCESS ) {
-// 		std::cout << "All tests are OK" << std::endl;
-// 	} else {
-// 		std::cout << "Any test/s are NG" << std::endl;
-// 	}
-// 	return exit_code;
-// }
